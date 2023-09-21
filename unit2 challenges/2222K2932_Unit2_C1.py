1bdf6affe403a57e01e83c9a9dc58dd38b08b687
2222k2932 <144433485+2222k2932@users.noreply.github.com>
2023-09-21 17:52:17 +0530

    2222K2932_Unit2_C1.py

--- unit2 challenges/2222K2932_Unit2_C1.py
+++ unit2 challenges/2222K2932_Unit2_C1.py
@@ -7,26 +7,25 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.__account_balance += amount
-            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+            print(f"Deposited ₹{amount}. New balance: ₹{self.__account_balance}")
         else:
             print("Invalid deposit amount. Please enter a positive amount.")
 
     def withdraw(self, amount):
         if amount > 0 and amount <= self.__account_balance:
             self.__account_balance -= amount
-            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+            print(f"Withdrew ₹{amount}. New balance: ₹{self.__account_balance}")
         elif amount > self.__account_balance:
             print("Insufficient funds. Withdrawal not allowed.")
         else:
             print("Invalid withdrawal amount. Please enter a positive amount.")
 
     def display_balance(self):
-        print(f"Account Balance for {self.__account_holder_name} (Account #: {self.__account_number}): ${self.__account_balance}")
+        print(f"Account Balance for {self.__account_holder_name} (Account #: {self.__account_number}): ₹{self.__account_balance}")
 
 
 # Create an instance of the BankAccount class
-account1 = BankAccount("12345", "John Doe", 1000)
-
+account1 = BankAccount("12345", "DHAANUSH S B", 5000)
 # Test deposit and withdrawal functionality
 account1.display_balance()
 account1.deposit(500)
